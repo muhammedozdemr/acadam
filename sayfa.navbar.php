db7623777257cbca3e4554e700b961177158f205
Nuri Akman <nuriakman@gmail.com>
2019-08-01 22:02:47 +0300

    Navbar linkleri güncellendi

--- sayfa.navbar.php
+++ sayfa.navbar.php
@@ -1,35 +1,31 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Aç Adam</a>
+  <a class="navbar-brand" href="index.php">Aç Adam!</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Ana Sayfa <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
+      <li class="nav-item active"> <a class="nav-link" href="index.php">Ana Sayfa</a> </li>
+      <li class="nav-item active"> <a class="nav-link" href="index.php?sayfa=musteri-kayit">Müşteri Kayıt</a> </li>
+      <li class="nav-item active"> <a class="nav-link" href="index.php?sayfa=esnaf-kayit">Esnaf Kayıt</a> </li>
+      <li class="nav-item active"> <a class="nav-link" href="index.php?sayfa=iletisim-formu">İletişim Formu</a> </li>
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Diğer Sayfalar
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Sayfa 1</a>
+          <a class="dropdown-item" href="#">Sayfa 2</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Sayfa 3</a>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="search" placeholder="Esnaf Ara" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ara!</button>
     </form>
   </div>
 </nav>
