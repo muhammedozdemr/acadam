dbe94c10aa2fbfbb85ba1e958260dfb741dba2a9
Nuri Akman <nuriakman@gmail.com>
2019-08-01 22:01:24 +0300

    Ana sayfa çalışma altyapısı hazırlandı

--- index.php
+++ index.php
@@ -11,10 +11,22 @@
   // Sayfanın ÜST kısmını alalım
   require("sayfa.ust.php");
 
-  // Ana sayfayı çağıralım
-  require("musteri.kayit.formu.php");
 
-  //require("ana.sayfa.php");
+  ####################### HANGİ SAYFANIN GÖSTERİLECEĞİNİN AYARLANMASI
+  ####################### HANGİ SAYFANIN GÖSTERİLECEĞİNİN AYARLANMASI
+  ####################### HANGİ SAYFANIN GÖSTERİLECEĞİNİN AYARLANMASI
+
+      $SAYFA = $GENEL_VarsayilanSayfa; // Gösterilecek varsayılan sayfayı ilk değer olarak atayalım.
+
+      if( isset($_GET["sayfa"]) ) $SAYFA = $_GET["sayfa"]; // Varsa, URL'den gelen değişkeni al
+
+      if( isset($GENEL_arrSAYFALAR[$SAYFA]) ) { // Tanımlı sayfalarda bu sayfa var mı?
+        require($GENEL_arrSAYFALAR[$SAYFA]); // Bu sayfa varsa, içeriğini göster
+      } else {
+        require($GENEL_VarsayilanSayfa); // Yoksa, varsayılan sayfayı gösterelim.
+      }
+
+
 
   // Sayfanın alt kısmını alalım
   require("sayfa.alt.php");
