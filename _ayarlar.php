b5c25e7133d34d04bc9dc752a0c862097aeb0a7b
Nuri Akman <nuriakman@gmail.com>
2019-08-01 21:56:37 +0300

    Dosya adları yeniden düzenlendi

---
+++ _ayarlar.php
@@ -0,0 +1,23 @@
+<?php
+
+  ############### Veritabanı Bağlantı Ayarları
+  ############### Veritabanı Bağlantı Ayarları
+  $SunucuAdi    = "localhost";
+  $KullaniciAdi = "root";
+  $Parola       = "root";
+  $VeritabaniAdi= "ac_adam";
+
+
+  ############### Sistem genelinde kullanılacak değişkenkler
+  ############### Sistem genelinde kullanılacak değişkenkler
+  $GENEL_sayfa_basligi = "Aç Adam!";
+
+  $GENEL_VarsayilanSayfa = "karsilama-sayfasi";
+  $GENEL_arrSAYFALAR = array(
+                          "karsilama-sayfasi"=> "karsilama.sayfasi.php",
+                          "musteri-kayit"    => "musteri.kayit.formu.php",
+                          "esnaf-kayit"      => "esnaf.kayit.formu.php",
+                          "iletisim-formu"   => "iletisim.formu.php"
+                       );
+
+?>
--- ana.sayfa.php
+++
@@ -1,3 +0,0 @@
-<h1>Aç Adam Ana Sayfası</h1>
-
-<a href="musteri.kayit.formu.php">Yeni Müşteri Kayıt</a>
